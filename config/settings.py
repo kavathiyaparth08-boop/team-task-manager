a6c7d2ea6fed67baec6f20479253bdf8e182eac5
parthkavathiya26 <pckavathiya1@gmail.com>
2026-05-03 22:18:52 +0530

    connect postgres

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from datetime import timedelta
 import os
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -60,10 +61,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # ✅ DATABASE
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
 }
 
 # ✅ PASSWORD VALIDATION
